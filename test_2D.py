b8fd5d886b0bc30e6ce99e28c7b7764d9cea1c74
fleaxiao <fleaxiao@gmail.com>
2023-12-19 15:32:26 +0100

    update

--- test_2D.py
+++ test_2D.py
@@ -99,51 +99,51 @@ def main():
     print(test_outputs_max)
     print(test_outputs_min)
 
-#     if torch.cuda.is_available():
-#         kwargs = {'num_workers': 0, 'pin_memory': True, 'pin_memory_device': "cuda"}
-#     else:
-#         kwargs = {'num_workers': 0, 'pin_memory': True}
-#     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False, **kwargs)
+    if torch.cuda.is_available():
+        kwargs = {'num_workers': 0, 'pin_memory': True, 'pin_memory_device': "cuda"}
+    else:
+        kwargs = {'num_workers': 0, 'pin_memory': True}
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False, **kwargs)
 
-#     net = Net().to(device)
-#     net.load_state_dict(torch.load('results_IW/Model_2D_IW_10.pth', map_location = torch.device('cpu')))
+    net = Net().to(device)
+    net.load_state_dict(torch.load('results_IW/Model_2D_IW_10.pth', map_location = torch.device('cpu')))
 
-#   # Evaluation
-#     net.eval()
-#     x_meas = []
-#     y_meas = []
-#     y_pred = []
-#     with torch.no_grad():
-#         for inputs, labels in test_loader:
-#             y_pred.append(net(inputs.to(device)))
-#             y_meas.append(labels.to(device))
-#             x_meas.append(inputs)
+  # Evaluation
+    net.eval()
+    x_meas = []
+    y_meas = []
+    y_pred = []
+    with torch.no_grad():
+        for inputs, labels in test_loader:
+            y_pred.append(net(inputs.to(device)))
+            y_meas.append(labels.to(device))
+            x_meas.append(inputs)
 
-#     y_meas = torch.cat(y_meas, dim=0)
-#     y_pred = torch.cat(y_pred, dim=0)
-#     print(f"Test Loss: {F.mse_loss(y_meas, y_pred).item() / len(test_dataset) * 1e5:.5f}")  # f denotes formatting string
+    y_meas = torch.cat(y_meas, dim=0)
+    y_pred = torch.cat(y_pred, dim=0)
+    print(f"Test Loss: {F.mse_loss(y_meas, y_pred).item() / len(test_dataset) * 1e5:.5f}")  # f denotes formatting string
     
-#     # tensor is transferred to numpy
-#     yy_pred = y_pred.cpu().numpy()
-#     yy_meas = y_meas.cpu().numpy()
+    # tensor is transferred to numpy
+    yy_pred = y_pred.cpu().numpy()
+    yy_meas = y_meas.cpu().numpy()
 
-#     yy_pred = yy_pred * (test_outputs_max - test_outputs_min) + test_outputs_min
-#     yy_meas = yy_meas * (test_outputs_max - test_outputs_min) + test_outputs_min
+    yy_pred = yy_pred * (test_outputs_max - test_outputs_min) + test_outputs_min
+    yy_meas = yy_meas * (test_outputs_max - test_outputs_min) + test_outputs_min
 
-#     yy_pred = 10**yy_pred
-#     yy_meas = 10**yy_meas
+    yy_pred = 10**yy_pred
+    yy_meas = 10**yy_meas
 
-#     # Relative Error
-#     Error_re = np.zeros_like(yy_meas)
-#     Error_re[yy_meas != 0] = abs(yy_pred[yy_meas != 0] - yy_meas[yy_meas != 0]) / abs(yy_meas[yy_meas != 0]) * 100
-#     # Error_re = np.squeeze(Error_re, axis=0)
+    # Relative Error
+    Error_re = np.zeros_like(yy_meas)
+    Error_re[yy_meas != 0] = abs(yy_pred[yy_meas != 0] - yy_meas[yy_meas != 0]) / abs(yy_meas[yy_meas != 0]) * 100
+    # Error_re = np.squeeze(Error_re, axis=0)
 
-#     Error_re_avg = np.mean(Error_re)
-#     Error_re_rms = np.sqrt(np.mean(Error_re ** 2))
-#     Error_re_max = np.max(Error_re)
-#     print(f"Relative Error: {Error_re_avg:.8f}%")
-#     print(f"RMS Error: {Error_re_rms:.8f}%")
-#     print(f"MAX Error: {Error_re_max:.8f}%")
+    Error_re_avg = np.mean(Error_re)
+    Error_re_rms = np.sqrt(np.mean(Error_re ** 2))
+    Error_re_max = np.max(Error_re)
+    print(f"Relative Error: {Error_re_avg:.8f}%")
+    print(f"RMS Error: {Error_re_rms:.8f}%")
+    print(f"MAX Error: {Error_re_max:.8f}%")
    
 if __name__ == "__main__":
     main()
